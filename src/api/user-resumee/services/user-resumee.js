bc796357762908698d6aca071044355008f7d2d9
Arpit Rai <arpitr2002@outlook.com>
2024-10-30 01:24:50 +0530

    CustemCV Ai BackEnd

---
+++ src/api/user-resumee/services/user-resumee.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-resumee service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::user-resumee.user-resumee');
